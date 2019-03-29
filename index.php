21fec6cb35577efd19760ef8c50b8e0ed64aea75
OlgaNesteruk <olganesteruk1012@gmail.com>
2019-03-29 21:22:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ function return_descriptor_img($url, $alt=null,
+ $height=null, $width=null)
+{
+  $html = '<img src="' . $url . '"';
+    if (isset($alt)) {
+      $html .=' alt="' . $alt .'"';// code...
+    }
+    if (isset($height)) {
+    $html .=' height="' . $height .'"';
+    }
+    if (isset($width)) {
+      $html .=' width="' . $width . '"';
+    }
+  $html .='/>';
+  return $html;
+}
+
+echo "Hello"; //comment 
+
+?>
