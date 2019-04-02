1484d1de10b824384065bbb7adebb565c7cf4348
OlgaNesteruk <olganesteruk1012@gmail.com>
2019-04-02 16:00:11 +0300

    index.php

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
- function return_descriptor_img($url, $alt=null,
- $height=null, $width=null)
-{
-  $html = '<img src="' . $url . '"';
-    if (isset($alt)) {
-      $html .=' alt="' . $alt .'"';// code...
-    }
-    if (isset($height)) {
-    $html .=' height="' . $height .'"';
-    }
-    if (isset($width)) {
-      $html .=' width="' . $width . '"';
-    }
-  $html .='/>';
-  return $html;
-}
+$file = fopen('feed.csv', "r");
 
+		echo '<table cellspacing = "0" border = "1" width = "500">';
+		while(!feof($file)) {
+			$mass = fgetcsv($file, 2000);
+			echo '<tr aling = "center">';
+			echo '<td width = "25%">';
+			echo $mass[4];
+			echo '</td>';
+			echo '<td width = "25%">';
+			echo $mass[19];
+			echo '</td>';
+if ($mass[4]<$mass[19]) {
+	echo '<td width = "25%">';
+	  	  echo "$mass[12]";// code...
+				echo '</td>';
+					echo '</tr>';
+} else {
+	echo '<td width = "25%">';
+	echo '</td>';// code...
+}
+}
+		echo '</table>';
+		fclose($file);
 ?>
