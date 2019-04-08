c611fc019ca7821e26e5290649e3822612fc6873
OlgaNesteruk <olganesteruk1012@gmail.com>
2019-04-08 15:14:03 +0300

    increased price for pants

--- index.php
+++ index.php
@@ -6,21 +6,23 @@ $file = fopen('feed.csv', "r");
 			$mass = fgetcsv($file, 2000);
 			echo '<tr aling = "center">';
 			echo '<td width = "25%">';
+			echo $mass[1];
+			echo '</td>';
+			$pants = 'PANTS';
+			$find = stripos($mass[1], $pants);
+		if ($find === false) {
+			echo '<td width = "25%">';
 			echo $mass[4];
 			echo '</td>';
+		}else {
 			echo '<td width = "25%">';
-			echo $mass[19];
+			$price_more_pants=$mass[4]*40/100+$mass[4];
+			echo $price_more_pants;
 			echo '</td>';
-if ($mass[4]<$mass[19]) {
-	echo '<td width = "25%">';
-	  	  echo "$mass[12]";// code...
-				echo '</td>';
-					echo '</tr>';
-} else {
-	echo '<td width = "25%">';
-	echo '</td>';// code...
-}
+		}
+			//var_dump($mass[1]);
 }
 		echo '</table>';
 		fclose($file);
+		
 ?>
